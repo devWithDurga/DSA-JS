c5d5f79e274bd9c5c4e76eb4fa99ef95d5bf248a
durga <durga1997rao58@gmail.com>
2025-11-02 18:00:30 +0530

    added one more exampole in day 1

--- Day1.js
+++ Day1.js
@@ -27,3 +27,15 @@ function LargestNumber(arr) {
 }
 console.log("LargestNumber", LargestNumber(arr));
 
+//find a number in array which has smallest number in array
+
+function Smallest(arr) {
+  let smallest = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i]
+    }
+  }
+  return smallest;
+}
+console.log("Smallest", Smallest(arr));
